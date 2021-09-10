fc5305cc551c39ee2ad66846c93e15d6d2d3d81e
hi33741 <hi33741@naver.com>
2021-09-10 11:33:37 +0900

    menu scroll

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -9,3 +9,19 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+//handle scroll
+
+const navbarMenu = document.querySelector(".navbar__menu");
+navbar.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+
+  if (link === undefined) {
+    return;
+  } else {
+    console.log(event.target.dataset.link);
+  }
+  const scrollId = document.querySelector(link);
+  scrollId.scrollIntoView({ behavior: "smooth" });
+});
